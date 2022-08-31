d5cc45896ab4a82185cd75bdf079ac71882e1de0
buga <swd543@gmail.com>
2022-08-31 23:51:21 +0200

    Initial commit

---
+++ hello/target/debug/deps/hello-20c6849f6460fc9d.d
@@ -0,0 +1,5 @@
+/home/buga/code/rusty/rusty-play/hello/target/debug/deps/hello-20c6849f6460fc9d.rmeta: src/main.rs
+
+/home/buga/code/rusty/rusty-play/hello/target/debug/deps/hello-20c6849f6460fc9d.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-8f887bad9b7694b9.d
@@ -0,0 +1,5 @@
+/home/buga/code/rusty/rusty-play/hello/target/debug/deps/hello-8f887bad9b7694b9.rmeta: src/main.rs
+
+/home/buga/code/rusty/rusty-play/hello/target/debug/deps/hello-8f887bad9b7694b9.d: src/main.rs
+
+src/main.rs:
